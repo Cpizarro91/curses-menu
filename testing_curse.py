2412e756b1a95d7f5bb0981da22e0a72497287f3
Cpizarro91 <cassandra.pizarro@gmail.com>
2017-04-20 18:50:58 -0400

    Update testing curse file to append all item names

--- testing_curse.py
+++ testing_curse.py
@@ -1,7 +1,9 @@
 # Import the necessary packages
 from cursesmenu import *
 from cursesmenu.items import *
+#from log import log
 
+names2 = []
 # Create the menu
 menu = CursesMenu("Title", "Subtitle")
 
@@ -29,5 +31,12 @@ menu.append_item(function_item)
 menu.append_item(command_item)
 menu.append_item(submenu_item)
 
+for x in range(0, len(menu.get_namesarray())):
+    names2.append(menu.get_namesarray()[x])
+
+selection_menu2 = SelectionMenu(names2)
+submenu_item2 = SubmenuItem("submenu2!", selection_menu2, menu)
+
+menu.append_item(submenu_item2)
 # Finally, we call show to show the menu and allow the user to interact
 menu.show()
